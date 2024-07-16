44cf1becbe0a76fa868b5fb78c9122a519c885ed
dinhphi0899 <dinhphi9c@gmail.com>
2024-07-16 22:17:13 +0700

    fix css

--- baiTap2/indexBai2.js
+++ baiTap2/indexBai2.js
@@ -6,7 +6,10 @@ btnTotal.addEventListener('click', () => {
     let ngayLam = ngayLamInput.value * 1
     let mucLuong = mucLuongInput.value * 1
     let total = Number(ngayLam) * Number(mucLuong)
-    console.log(total)
-    return document.getElementById('total').innerHTML = total
+    const formattedBonus = new Intl.NumberFormat('vi-VN', {
+        style: 'currency',
+        currency: 'VND'
+    }).format(total);
+    return document.getElementById('total').innerHTML = formattedBonus
 })
 
